b7369dbb0a58c6325a377536441d5f8bd33658ba
sunghuncho <chohoon901@gmail.com>
2023-04-20 15:42:39 +0900

    sol4 new

---
+++ src/datatype/Solution5.java
@@ -0,0 +1,31 @@
+
+package datatype;
+
+public class Solution5 {
+    public static void main(String[] args) {
+        String str = "100";
+        System.out.println("str.getClass() = " + str.getClass());
+        int num = Integer.parseInt(str);
+        System.out.println("num = " + num);
+
+        System.out.println("Integer.parseInt(\"-100\") = " + Integer.parseInt("-100"));
+        int num2 = Integer.parseInt("-100");
+        System.out.println("num2 + 100 = " + (num2 + 100));
+        System.out.println("Double.parseDouble(\"-100.5\") = " + Double.parseDouble("-100.5"));
+        double num3  = Double.parseDouble("100.5");
+        System.out.println("num3 + 100 = " + (num3 + 100));
+
+//        System.out.println("Integer.parseInt(\"hundred\") = " + Integer.parseInt("hundred"));
+
+        int num4 = 200;
+//        System.out.println("num4.toString()= " + new (Integer)num4.toString());
+
+        String str2 = "200";
+        int num5 = 300;
+        System.out.println("(double)num5 = " + (double)num5);
+
+        double num6 = 300;
+        System.out.println("(int)num6 = " + (int)num6);
+
+    }
+}
---
+++ src/datatype/Solution7.java
@@ -0,0 +1,13 @@
+
+package datatype;
+
+public class Solution7 {
+    public static void main(String[] args) {
+        int score = 100;
+        score = 50;
+        System.out.println("score = " + score);
+        final int SCORE = 100; // const
+        System.out.println("SCORE = " + SCORE);
+
+    }
+}
